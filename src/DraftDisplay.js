3c45be05a14761662ae6fa4f66a989dc0642347f
aidenspro <aidenspro@gmail.com>
2022-03-11 15:39:15 -0500

    1 file modified, 1 added

--- src/DraftDisplay.js
+++ src/DraftDisplay.js
@@ -1,35 +1,37 @@
 import React, { useState, useEffect, useRef, createRef } from 'react';
-import CreateCard from './CreateCard'
+import CreateCard from './CreateCard';
+import DraftDisplay from './DraftDisplay';
 
-let packNum = 1;//number of the current card set
+let packNum = 1; //number of the current card set
 
-export default function draftDisplay(props){
-  
+export default function draftDisplay(props) {
   //when updated reloads the createCard component for a new card
-  const [changeCard,setChangeCard] = useState(0);
-
+  const [changeCard, setChangeCard] = useState(0);
 
   //passed to cards to allow them to load more cards when clicked
-  const handleOnClick= () => {
-    setChangeCard(packNum)//reload card component
-    packNum++;//update pack#
-  }
+  const handleOnClick = () => {
+    setChangeCard(packNum); //reload card component
+    packNum++; //update pack#
+  };
 
   const getCards = () => {
     const cardArray = [];
-    for(let i =0; i < props.cardsPerPack ; i++)
-      cardArray[i] = <CreateCard key={i} changeCard={changeCard} handleOnClick={handleOnClick}/>
+    for (let i = 0; i < props.cardsPerPack; i++)
+      cardArray[i] = (
+        <CreateCard
+          key={i}
+          changeCard={changeCard}
+          handleOnClick={handleOnClick}
+        />
+      );
 
     return cardArray;
-  }
+  };
 
-
-  return(
+  return (
     <div>
-      <div className='cardContainer'>
-        {getCards()}
-      </div>
+      <div className="cardContainer">{getCards()}</div>
+      <DraftDisplay />
     </div>
-  )
-
+  );
 }
---
+++ src/DraftedCards.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect, useRef, createRef } from 'react';
+
+export default function draftedCards() {
+
+
+  return(
+    5
+  )
+}
