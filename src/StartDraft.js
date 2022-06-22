ae8ac4b46828ee5882ef375edb045efd08d48466
aidenspro <aidenspro@gmail.com>
2022-06-22 14:42:08 -0400

    1 file modified

--- src/StartDraft.js
+++ src/StartDraft.js
@@ -11,9 +11,9 @@ const [extraDeckPacks, setExtraDeckPacks] = useState(5);
 if(start == 0)
 return(
   <div className={'startDiv'} >
-  <SettingsButtons text={"Number of Cards Per Pack"} setSetting={setCardsPerPack} values={[2,5,10]} artwork={1}/>
-  <SettingsButtons text={"Number of Packs Per Draft"} setSetting={setNumberOfPacks} values={[2,5,10]} artwork={2}/>
-  <SettingsButtons text={"Number of Extra-Packs Per Draft"} setSetting={setExtraDeckPacks} values={[2,5,10]} artwork={3}/>
+  <SettingsButtons text={"Number of Cards Per Pack"} setSetting={setCardsPerPack} values={[5,7,10]} artwork={1}/>
+  <SettingsButtons text={"Number of Packs Per Draft"} setSetting={setNumberOfPacks} values={[10,20,25]} artwork={2}/>
+  <SettingsButtons text={"Number of Extra-Packs Per Draft"} setSetting={setExtraDeckPacks} values={[5,10,15]} artwork={3}/>
   <button className='startDraft'onClick={() => setStart(1)}> Start Draft </button>
   </div>)
 else
